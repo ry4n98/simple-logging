81d01d9077a927cec4bc2f422cfc9dcb07f2845c
Ryan <ryan.smith4@sky.uk>
2018-01-07 12:21:58 +0000

    Initial commit

---
+++ simple-logging.js
@@ -0,0 +1,29 @@
+const INFO_TEXT = "[INFO]"
+const WARN_TEXT = "[WARN]"
+const CRIT_TEXT = "[CRIT]"
+const SPACE = " "
+
+function timestamp() {
+  var d = new Date()
+  var ts = d.toLocaleTimeString()
+  ts = `[${ts}]`
+  return ts
+}
+
+function info() {
+  return INFO_TEXT + timestamp() + SPACE;
+}
+
+function warn() {
+  return WARN_TEXT + timestamp() + SPACE;
+}
+
+function crit() {
+  return CRIT_TEXT + timestamp() + SPACE;
+}
+
+module.exports = {
+  info: info,
+  warn: warn,
+  crit: crit,
+}
